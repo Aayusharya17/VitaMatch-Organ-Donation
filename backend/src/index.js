fc1fb266129559f22ecf43827167e0d55e4ceb68
dhiman31 <ankushiiituna@gmail.com>
2026-01-27 21:51:06 +0530

    Added signup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const { PORT } = require('./config/serverConfig');
 
 setUpAndStartServer = () => {
-    app.listen(3000 , ()=> {
-        console.log("Server started on PORT 3000");
+
+    const app = express();
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    app.listen(PORT , ()=> {
+        console.log("Server started on PORT ",PORT);
     })
 }
