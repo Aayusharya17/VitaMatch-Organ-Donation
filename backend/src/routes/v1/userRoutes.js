98d289a9fdbdc9ef813eecd3e9eaaeadf3faed39
dhiman31 <ankushiiituna@gmail.com>
2026-01-27 22:34:03 +0530

    Login completed

--- backend/src/routes/v1/userRoutes.js
+++ backend/src/routes/v1/userRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../../controllers/userController');
 
-router.post('/',userController.signup);
+router.post('/signup',userController.signup);
+router.post('/login',userController.login);
 
 module.exports = router;
