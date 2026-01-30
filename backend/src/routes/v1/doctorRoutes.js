d699da05f32ea23654d17de3b54835144dd3767f
dhiman31 <ankushiiituna@gmail.com>
2026-01-30 20:42:26 +0530

    Fixed errors

--- backend/src/routes/v1/doctorRoutes.js
+++ backend/src/routes/v1/doctorRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const doctorController = require('../../controllers/doctorController');
 const {authMiddleware} = require('../../middleware/auth');
 
-router.post('/requestOrgan',authMiddleware,doctorController.requestedOrgan);
+router.post('/requestOrgan',authMiddleware,doctorController.requestOrgan);
 router.get('/availableOrgans',authMiddleware,doctorController.findAllAvailable);
 router.post("/accept-organ", authMiddleware, doctorController.acceptOrgan);
 
